6596bc3518a6f2b6c4f468ae26dfdaab5b84dccd
leon mak <leonmakansheng@gmail.com>
2017-05-13 20:11:19 +0800

    Site updated: 2017-05-13 20:11:18

---
+++ resume-react/babel/index.js
@@ -0,0 +1,169 @@
+const {Motion, spring, TransitionMotion} = ReactMotion
+
+var commentData = [
+	{author: 'Waympus Loonger', text: 'look at this comment!'},
+	{author: 'Donderk Sept', text: 'No, this comment is more interesting!'},
+	{author: 'Lentfop Abdus', text: 'I can say a thing here?'},
+	{author: 'Stenin Bumk', text: 'Why is wall though?'},
+	{author: 'Stenin Bumk', text: 'Waldo*'},
+	{author: 'Hastov Lennonbrook', text: 'H- How did a typo of Waldo turn into wall though???'},
+	{author: 'Lentfop Abdus', text: '^ this guy'},
+	{author: 'Stenin Bumk', text: 'It sounds similar!'},
+	{author: 'Henchtop X. Splathurdin', text: 'y\'all are freaky.'},
+	{author: 'Snecknope Antlebee', text: 'Guys guys guys!'},
+	{author: 'Lentfop Abdus', text: 'Jeez what?'},
+	{author: 'Snecknope Antlebee', text: 'so they say comedy is observation plus time, right?'},
+	{author: 'xXx_Ethan_xXx', text: 'I\'ll bite.'},
+	{author: 'xXx_Ethan_xXx', text: 'Yeah that\'s what they say...'},
+	{author: 'Snecknope Antlebee', text: 'Yeah, but then I got to thinking. Jerry Sienfeld showed us that observation is also comedy!'},
+	{author: 'Jaymus Flabbergasser', text: 'SEVENTEEN!!!!'},
+	{author: 'Snecknope Antlebee', text: 'It\'s basic algebra.'},
+	{author: 'Snecknope Antlebee', text: 'tragedy + time = comedy'},
+	{author: 'Snecknope Antlebee', text: 'observations = comedy'},
+	{author: 'Snecknope Antlebee', text: 'thus!!! tragedy = observation - time'},
+	{author: 'Snecknope Antlebee', text: 'in lame-man-speak timeless observation is equal to tragedy'}
+]
+
+const springPreset = {
+	wobbly: [120,11]
+}
+
+
+
+class CommentList extends React.Component {
+	componentDidMount() {
+		this.comDisplay = $('.comment-list')
+	}
+	componentDidUpdate() {
+		let comHeight = this.comDisplay.outerHeight()
+		let comScroll = this.comDisplay[0].scrollHeight
+		
+		this.comDisplay.stop().animate({'scrollTop': comScroll - comHeight}, 200)
+	};
+	getStyles() {
+		let configs = {}
+		this.props.data.forEach((val,index) => {
+			configs[val.id] = {
+				opacity: spring(1),
+				top: spring(0, springPreset.wobbly)
+			}
+		})
+		return configs
+	};
+	willEnter(key) {
+		return {
+			opacity: spring(0),
+			top: spring(100, springPreset.wobbly)
+		}
+	};
+	render() {
+		return (
+			<TransitionMotion
+				styles={this.getStyles()}
+				willEnter={this.willEnter}
+				willLeave={this.willLeave}>
+				{interp =>
+					<div className='comment-list'>
+						{this.props.data.map((comment, i) => {
+							const {...style} = interp[i + 1];
+							return (
+								<div className='comment-node' author={comment.author} key={comment.id} style={style}>
+									<div className='print-author'>
+										{comment.author + ' - '}
+									</div> 
+									{comment.text}
+								</div>
+							)
+						})}
+					</div>
+				}
+			</TransitionMotion>
+		)
+	};
+}
+
+class CommentForm extends React.Component {
+	constructor() {
+		super()
+		this.state = {author: '', text: '', canPost: 'disabled'}
+	};
+	handleAuthorChange = (e) =>{
+		this.setState({author: e.target.value})
+	};
+	handleTextChange = (e) => {
+		this.setState({text: e.target.value})
+	};
+	handleSubmit = (e) => {
+		e.preventDefault()
+		var author = this.state.author.trim(),
+			text = this.state.text.trim()
+		if(!text || !author) {
+			return
+		}
+		this.props.onCommentSubmit({author: author, text: text})
+		this.setState({text: ''})
+	};
+	render() {
+		return (
+			<form className='comment-form' onSubmit={this.handleSubmit}>
+				<input
+					type='text'
+					placeholder='Name'
+					value={this.state.author}
+					onChange={this.handleAuthorChange}
+					maxLength='20'
+					className='name-input'
+					required
+				/>
+				<input 
+					type='text'
+					placeholder='Comment'
+					value={this.state.text}
+					onChange={this.handleTextChange}
+				/>
+				<input 
+					type='submit'
+					value='Post'
+					disabled={!this.state.author.trim() || !this.state.text.trim()}
+				/>
+			</form>
+		)
+	};
+};
+
+class CommentBox extends React.Component {
+	constructor() {
+		super()
+		this.state = {data: []}
+	}
+	componentDidMount() {
+		this.addRandomComment()
+	}
+	handleCommentSubmit = (comment) => {
+		comment['id'] = this.state.data.length + 1
+		this.setState({data: this.state.data.concat(comment)})
+	};
+	addRandomComment = () => {
+		this.handleCommentSubmit(commentData[0])
+		commentData.splice(0,1)
+		if(commentData.length > 0) {
+			let cTime = Math.random() * 7000 + 500
+			setTimeout(this.addRandomComment, cTime)
+		}
+	};
+	render() {
+		return (
+			<div className='comment-box'>
+				<h1>Comments</h1>
+				<CommentList data={this.state.data}/>
+				<CommentForm onCommentSubmit={this.handleCommentSubmit}/>
+
+			</div>
+		)
+	};
+}
+
+ReactDOM.render(
+	<CommentBox/>,
+	document.getElementById('react-app')
+)
--- resume-react/js/index.js
+++ resume-react/js/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -8,10 +10,11 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var _ReactMotion = ReactMotion;
-var Motion = _ReactMotion.Motion;
-var spring = _ReactMotion.spring;
-var TransitionMotion = _ReactMotion.TransitionMotion;
+var _ReactMotion = ReactMotion,
+    Motion = _ReactMotion.Motion,
+    spring = _ReactMotion.spring,
+    TransitionMotion = _ReactMotion.TransitionMotion;
+
 
 var commentData = [{ author: 'Waympus Loonger', text: 'look at this comment!' }, { author: 'Donderk Sept', text: 'No, this comment is more interesting!' }, { author: 'Lentfop Abdus', text: 'I can say a thing here?' }, { author: 'Stenin Bumk', text: 'Why is wall though?' }, { author: 'Stenin Bumk', text: 'Waldo*' }, { author: 'Hastov Lennonbrook', text: 'H- How did a typo of Waldo turn into wall though???' }, { author: 'Lentfop Abdus', text: '^ this guy' }, { author: 'Stenin Bumk', text: 'It sounds similar!' }, { author: 'Henchtop X. Splathurdin', text: 'y\'all are freaky.' }, { author: 'Snecknope Antlebee', text: 'Guys guys guys!' }, { author: 'Lentfop Abdus', text: 'Jeez what?' }, { author: 'Snecknope Antlebee', text: 'so they say comedy is observation plus time, right?' }, { author: 'xXx_Ethan_xXx', text: 'I\'ll bite.' }, { author: 'xXx_Ethan_xXx', text: 'Yeah that\'s what they say...' }, { author: 'Snecknope Antlebee', text: 'Yeah, but then I got to thinking. Jerry Sienfeld showed us that observation is also comedy!' }, { author: 'Jaymus Flabbergasser', text: 'SEVENTEEN!!!!' }, { author: 'Snecknope Antlebee', text: 'It\'s basic algebra.' }, { author: 'Snecknope Antlebee', text: 'tragedy + time = comedy' }, { author: 'Snecknope Antlebee', text: 'observations = comedy' }, { author: 'Snecknope Antlebee', text: 'thus!!! tragedy = observation - time' }, { author: 'Snecknope Antlebee', text: 'in lame-man-speak timeless observation is equal to tragedy' }];
 
@@ -25,69 +28,76 @@ var CommentList = function (_React$Component) {
 	function CommentList() {
 		_classCallCheck(this, CommentList);
 
-		return _possibleConstructorReturn(this, _React$Component.apply(this, arguments));
+		return _possibleConstructorReturn(this, (CommentList.__proto__ || Object.getPrototypeOf(CommentList)).apply(this, arguments));
 	}
 
-	CommentList.prototype.componentDidMount = function componentDidMount() {
-		this.comDisplay = $('.comment-list');
-	};
+	_createClass(CommentList, [{
+		key: 'componentDidMount',
+		value: function componentDidMount() {
+			this.comDisplay = $('.comment-list');
+		}
+	}, {
+		key: 'componentDidUpdate',
+		value: function componentDidUpdate() {
+			var comHeight = this.comDisplay.outerHeight();
+			var comScroll = this.comDisplay[0].scrollHeight;
 
-	CommentList.prototype.componentDidUpdate = function componentDidUpdate() {
-		var comHeight = this.comDisplay.outerHeight();
-		var comScroll = this.comDisplay[0].scrollHeight;
-
-		this.comDisplay.stop().animate({ 'scrollTop': comScroll - comHeight }, 200);
-	};
-
-	CommentList.prototype.getStyles = function getStyles() {
-		var configs = {};
-		this.props.data.forEach(function (val, index) {
-			configs[val.id] = {
-				opacity: spring(1),
-				top: spring(0, springPreset.wobbly)
+			this.comDisplay.stop().animate({ 'scrollTop': comScroll - comHeight }, 200);
+		}
+	}, {
+		key: 'getStyles',
+		value: function getStyles() {
+			var configs = {};
+			this.props.data.forEach(function (val, index) {
+				configs[val.id] = {
+					opacity: spring(1),
+					top: spring(0, springPreset.wobbly)
+				};
+			});
+			return configs;
+		}
+	}, {
+		key: 'willEnter',
+		value: function willEnter(key) {
+			return {
+				opacity: spring(0),
+				top: spring(100, springPreset.wobbly)
 			};
-		});
-		return configs;
-	};
+		}
+	}, {
+		key: 'render',
+		value: function render() {
+			var _this2 = this;
 
-	CommentList.prototype.willEnter = function willEnter(key) {
-		return {
-			opacity: spring(0),
-			top: spring(100, springPreset.wobbly)
-		};
-	};
+			return React.createElement(
+				TransitionMotion,
+				{
+					styles: this.getStyles(),
+					willEnter: this.willEnter,
+					willLeave: this.willLeave },
+				function (interp) {
+					return React.createElement(
+						'div',
+						{ className: 'comment-list' },
+						_this2.props.data.map(function (comment, i) {
+							var style = _objectWithoutProperties(interp[i + 1], []);
 
-	CommentList.prototype.render = function render() {
-		var _this2 = this;
-
-		return React.createElement(
-			TransitionMotion,
-			{
-				styles: this.getStyles(),
-				willEnter: this.willEnter,
-				willLeave: this.willLeave },
-			function (interp) {
-				return React.createElement(
-					'div',
-					{ className: 'comment-list' },
-					_this2.props.data.map(function (comment, i) {
-						var style = _objectWithoutProperties(interp[i + 1], []);
-
-						return React.createElement(
-							'div',
-							{ className: 'comment-node', author: comment.author, key: comment.id, style: style },
-							React.createElement(
+							return React.createElement(
 								'div',
-								{ className: 'print-author' },
-								comment.author + ' - '
-							),
-							comment.text
-						);
-					})
-				);
-			}
-		);
-	};
+								{ className: 'comment-node', author: comment.author, key: comment.id, style: style },
+								React.createElement(
+									'div',
+									{ className: 'print-author' },
+									comment.author + ' - '
+								),
+								comment.text
+							);
+						})
+					);
+				}
+			);
+		}
+	}]);
 
 	return CommentList;
 }(React.Component);
@@ -98,7 +108,7 @@ var CommentForm = function (_React$Component2) {
 	function CommentForm() {
 		_classCallCheck(this, CommentForm);
 
-		var _this3 = _possibleConstructorReturn(this, _React$Component2.call(this));
+		var _this3 = _possibleConstructorReturn(this, (CommentForm.__proto__ || Object.getPrototypeOf(CommentForm)).call(this));
 
 		_this3.handleAuthorChange = function (e) {
 			_this3.setState({ author: e.target.value });
@@ -123,32 +133,35 @@ var CommentForm = function (_React$Component2) {
 		return _this3;
 	}
 
-	CommentForm.prototype.render = function render() {
-		return React.createElement(
-			'form',
-			{ className: 'comment-form', onSubmit: this.handleSubmit },
-			React.createElement('input', {
-				type: 'text',
-				placeholder: 'Name',
-				value: this.state.author,
-				onChange: this.handleAuthorChange,
-				maxLength: '20',
-				className: 'name-input',
-				required: true
-			}),
-			React.createElement('input', {
-				type: 'text',
-				placeholder: 'Comment',
-				value: this.state.text,
-				onChange: this.handleTextChange
-			}),
-			React.createElement('input', {
-				type: 'submit',
-				value: 'Post',
-				disabled: !this.state.author.trim() || !this.state.text.trim()
-			})
-		);
-	};
+	_createClass(CommentForm, [{
+		key: 'render',
+		value: function render() {
+			return React.createElement(
+				'form',
+				{ className: 'comment-form', onSubmit: this.handleSubmit },
+				React.createElement('input', {
+					type: 'text',
+					placeholder: 'Name',
+					value: this.state.author,
+					onChange: this.handleAuthorChange,
+					maxLength: '20',
+					className: 'name-input',
+					required: true
+				}),
+				React.createElement('input', {
+					type: 'text',
+					placeholder: 'Comment',
+					value: this.state.text,
+					onChange: this.handleTextChange
+				}),
+				React.createElement('input', {
+					type: 'submit',
+					value: 'Post',
+					disabled: !this.state.author.trim() || !this.state.text.trim()
+				})
+			);
+		}
+	}]);
 
 	return CommentForm;
 }(React.Component);
@@ -161,7 +174,7 @@ var CommentBox = function (_React$Component3) {
 	function CommentBox() {
 		_classCallCheck(this, CommentBox);
 
-		var _this4 = _possibleConstructorReturn(this, _React$Component3.call(this));
+		var _this4 = _possibleConstructorReturn(this, (CommentBox.__proto__ || Object.getPrototypeOf(CommentBox)).call(this));
 
 		_this4.handleCommentSubmit = function (comment) {
 			comment['id'] = _this4.state.data.length + 1;
@@ -181,25 +194,30 @@ var CommentBox = function (_React$Component3) {
 		return _this4;
 	}
 
-	CommentBox.prototype.componentDidMount = function componentDidMount() {
-		this.addRandomComment();
-	};
-
-	CommentBox.prototype.render = function render() {
-		return React.createElement(
-			'div',
-			{ className: 'comment-box' },
-			React.createElement(
-				'h1',
-				null,
-				'Comments'
-			),
-			React.createElement(CommentList, { data: this.state.data }),
-			React.createElement(CommentForm, { onCommentSubmit: this.handleCommentSubmit })
-		);
-	};
+	_createClass(CommentBox, [{
+		key: 'componentDidMount',
+		value: function componentDidMount() {
+			this.addRandomComment();
+		}
+	}, {
+		key: 'render',
+		value: function render() {
+			return React.createElement(
+				'div',
+				{ className: 'comment-box' },
+				React.createElement(
+					'h1',
+					null,
+					'Comments'
+				),
+				React.createElement(CommentList, { data: this.state.data }),
+				React.createElement(CommentForm, { onCommentSubmit: this.handleCommentSubmit })
+			);
+		}
+	}]);
 
 	return CommentBox;
 }(React.Component);
 
 ReactDOM.render(React.createElement(CommentBox, null), document.getElementById('react-app'));
+
